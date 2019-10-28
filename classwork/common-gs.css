dc9823fad3af7022224b317f11d7d73c64cca6d6
Dina Ponomareva <ponomarevadina51@gmail.com>
2019-10-28 22:00:36 +0300

    три с половиной готовых блока второй страницы

---
+++ classwork/common-gs.css
@@ -0,0 +1,11 @@
+let isBurgerClicked = false;
+
+function burgerClick() {
+    if (isBurgerClicked) {
+        burger.classList.remove("is-active");
+    } else {
+        burger.classList.add("is-active");
+    }
+
+    isBurgerClicked = !isBurgerClicked;
+}
